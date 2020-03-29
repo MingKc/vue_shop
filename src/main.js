66a902f088e73a7627d00dfc4441ab8c0babda9d
陈柯明 <1129059298@qq.com>
2020-03-29 14:28:03 +0800

    实现了登录功能

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
 import './plugins/element.js'
+
+// 导入全局样式表
+import './assets/css/global.css'
+
+// 导入axios
+import axios from 'axios'
+// 在Vue原型挂载axios，并配置根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
